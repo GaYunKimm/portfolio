5a985053f1b41311a7935fd674a06399762cfc0a
GaYunKimm <gayoon109@gmail.com>
2022-03-20 22:19:16 +0900

    swiper slide 공통 적용
    배열 추가하여 슬라이드 공통 적용

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,12 +7,7 @@ const $swiperX = document.querySelectorAll(".slide-wrap")
 let prevYoffset; //스크롤 이전 
 let scrollDirection;//스크롤방향
 
-let touchable = false;
-let tPosX = {
-  start: 0,
-  current: 0,
-  end: 0,
-}
+
 let tPosY = {
   start: 0,
   end: 0,
@@ -60,55 +55,68 @@ function getTarget(elem, className) {
   return elem;
 }
 
-$contents.addEventListener("mousedown", (e) => {
-  let $swiper = getTarget(e.target, "slide-wrap");
-  $swiper.classList.add("nonetouch");
-  touchable = true;
-  tPosX.start = e.clientX;
+let touchable = [];
+let tPosX = [];
+let tIndex = [];
+
+$swiperX.forEach((swiper, idx) => {
+  tPosX[idx] = {
+    start: 0,
+    current: 0,
+    end: 0,
+  }
+  touchable[idx] = {};
+
+  swiper.addEventListener("mousedown", (e) => {
+    let $swiper = getTarget(e.target, "slide-wrap");
+    slideDown(e, idx, $swiper);
+  });
+
+  swiper.addEventListener("mousemove", (e) => {
+    let $swiper = getTarget(e.target, "slide-wrap");
+    slideMove(e, idx, $swiper);
+  });
+
+  swiper.addEventListener("mouseup", (e) => {
+    let $swiper = getTarget(e.target, "slide-wrap");
+    slideup(e, idx, $swiper);
+   // console.log(tPosX)
+  });
+
 });
-$contents.addEventListener("mousemove", (e) => {
-  let $swiper = getTarget(e.target, "slide-wrap");
-  if (!touchable && !$swiper.classList.contains("nonetouch")) return;
-  if ($swiper.scrollLeft <= 0 && tPosX.start < e.clientX) { //이전스크롤
-    tPosX.end = 0;
-    tPosX.current = 0;
-    tPosX.start = 0;
-  } else if ($swiper.scrollLeft === $swiper.querySelector('.slide-list').scrollWidth - $swiper.clientWidth&& tPosX.start > e.clientX) {
-    //tPosX.end = $swiper.querySelector('.slide-list').scrollWidth - $swiper.clientWidth;
-    tPosX.current = $swiper.querySelector('.slide-list').scrollWidth - $swiper.clientWidth;
+
+function slideDown(e, idx, $swiper) {
+  touchable[idx] = true;
+  tPosX[idx].start = e.clientX;
+}
+
+function slideMove(e, idx, $swiper) {
+
+  if (!touchable[idx] && !$swiper.classList.contains("nonetouch")) return;
+  $swiper.classList.add("nonetouch");
+  if ($swiper.scrollLeft <= 0 && tPosX[idx].start < e.clientX) { //이전스크롤
+    tPosX[idx] = {
+      start : 0,
+      current : 0,
+      end : 0,
+    }
+  } else if ($swiper.scrollLeft === $swiper.querySelector('.slide-list').scrollWidth - $swiper.clientWidth && tPosX[idx].start > e.clientX) {
+ 
+    tPosX[idx].current = $swiper.querySelector('.slide-list').scrollWidth - $swiper.clientWidth;
   }
   else {
-    tPosX.end = tPosX.current + (e.clientX - tPosX.start);//e.clientX;
+    tPosX[idx].end = tPosX[idx].current + (e.clientX - tPosX[idx].start);//e.clientX;
   }
-  $swiper.scrollTo(-tPosX.end, 0);
+  $swiper.scrollTo(-tPosX[idx].end, 0);
+}
 
-
-  // $swiper.style.transform = `translate3d(${tPosX.end}px,0,0)`
-  //console.log(tPosX);
-  //else if (startX - endX > 30) { }  //첫시작 - 뒷터치 = 왼쪽이동
-});
-$contents.addEventListener("mouseup", (e) => {
-  let $swiper = getTarget(e.target, "slide-wrap");
+function slideup(e, idx, $swiper) {
+  //let $swiper = getTarget(e.target, "slide-wrap");
   $swiper.classList.remove("nonetouch");
-  touchable = false;
-  tPosX.current = tPosX.end;
-  //console.log($swiper.scrollLeft, tPosX)
+  touchable[idx] = false;
+  tPosX[idx].current = tPosX[idx].end;
+}
 
-
-  console.log(tPosX);
-
-});
-
-
-// $contents.addEventListener("mousedown", swiper);
-// $contents.addEventListener("mousemove", swiper);
-// $contents.addEventListener("mouseup", swiper);
-
-// $contents.addEventListener("click",(e)=>{
-//   if(e.target.classList.contains("slide-wrap") ){
-
-//   }
-// })
 
 window.addEventListener('scroll', (e) => {
   if (prevYoffset > window.pageYOffset) {
